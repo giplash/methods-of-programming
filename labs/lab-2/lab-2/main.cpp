8e02d2fc49e2af7d0582b7fad87235d7f60ec067
giplash <anashkinviktar@gmail.com>
2018-09-26 18:59:48 +0300

    update lab-2

---
+++ labs/lab-2/lab-2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  lab-2
+//
+//  Created by Viktor Anashkin on 9/26/18.
+//  Copyright © 2018 BSU. All rights reserved.
+//
+
+#include <iostream>
+#include "Complex.h"
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    return 0;
+}
